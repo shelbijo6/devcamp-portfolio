85f811e9fed2471ee59d1ce143501c6ff1da31e2
Shelbi Schollenberger <sschollenberger6@gmail.com>
2017-03-07 09:47:13 -0700

    Implemented content tag helper methods

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,16 +1,16 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: [:show, :destroy, :edit, :update]
-  layout 'portfolio'
+  layout "portfolio"
   
   def index
     @portfolio_items = Portfolio.all
   end
-    
+  
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
-    
+  
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
     respond_to do |format|
@@ -21,10 +21,10 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-    
+  
   def edit
   end
-    
+  
   def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -45,18 +45,17 @@ class PortfoliosController < ApplicationController
     end
   end
   
-
-  private 
-   
+  private
+  
   def set_portfolio
     @portfolio_item = Portfolio.find(params[:id])
   end
-   
+  
   def portfolio_params
     params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
                                       technologies_attributes: [:name]
-                                      )
+    )
   end
 end
