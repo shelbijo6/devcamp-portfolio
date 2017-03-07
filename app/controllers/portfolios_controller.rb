677cbf9ef2008c601fe5cfa28c85aec429401109
Shelbi Schollenberger <sschollenberger6@gmail.com>
2017-03-06 21:58:49 -0700

    Implemented partials with localdata for portfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,69 +1,62 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio, only: [:edit, :update, :show, :destroy]
-  layout "portfolio"
+  before_action :set_portfolio, only: [:show, :destroy, :edit, :update]
+  layout 'portfolio'
   
   def index
     @portfolio_items = Portfolio.all
-  end 
-
-  def angular
-    @angular_portfolio_items = Portfolio.angular
-  end 
-
+  end
+    
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
-  end 
-
+  end
+    
   def create
-    @portfolio_item = Portfolio.new(portfolio_params)  
-
+    @portfolio_item = Portfolio.new(portfolio_params)
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portolio item is now live.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now online.' }
       else
         format.html { render :new }
-      end 
-    end 
-  end 
-
+      end
+    end
+  end
+    
   def edit
-  end  
-
+  end
+    
   def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
-
+  
   def show
   end
-
+  
   def destroy
-    # Destroy/delete the record
     @portfolio_item.destroy
-
-    # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
+  
 
-  private
+  private 
+   
   def set_portfolio
     @portfolio_item = Portfolio.find(params[:id])
-  end 
-
+  end
+   
   def portfolio_params
     params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
                                       technologies_attributes: [:name]
-                                     )
+                                      )
   end
 end
-  
