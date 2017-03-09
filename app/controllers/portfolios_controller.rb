1d3b8c45903fa9898208ec4817f9294dd15a0cd3
Shelbi Schollenberger <sschollenberger6@gmail.com>
2017-03-09 13:49:07 -0700

    Implemented authorization engine for protfolio items and blogs

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,30 +1,37 @@
+
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio, only: [:show, :destroy, :edit, :update]
-  layout "portfolio"
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
+  layout 'portfolio'
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
+
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now online.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -34,28 +41,31 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def show
   end
-  
+
   def destroy
+    # Destroy/delete the record
     @portfolio_item.destroy
+
+    # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
   private
-  
-  def set_portfolio
-    @portfolio_item = Portfolio.find(params[:id])
-  end
-  
+
   def portfolio_params
     params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
                                       technologies_attributes: [:name]
-    )
+                                     )
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
