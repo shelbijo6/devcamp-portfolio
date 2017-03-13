df4dab2e87dbcdfc4fbeb1369ef71ed183d20950
Shelbi Schollenberger <sschollenberger6@gmail.com>
2017-03-13 10:10:21 -0600

    Implemented refactor for nav and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,13 +10,53 @@ module ApplicationHelper
   end
 
   def source_helper(layout_name)
-    if session[:source] 
+    if session[:source]
       greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
-    end 
+    end
   end
 
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Shelbi Schollenberger', 'All rights reserved'
-  end 
-end 
+    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+  end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
+end
