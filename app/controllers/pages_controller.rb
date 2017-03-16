1178454078ec890565abcd796f8d58fbf17a18e0
Shelbi Schollenberger <sschollenberger6@gmail.com>
2017-03-16 14:39:05 -0600

    Learned how to add clickable links for each tweet. Decided that I didn't want that in my portfolio website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,8 +9,4 @@ class PagesController < ApplicationController
 
   def contact
   end
-
-  def tech_news
-    @tweets = SocialTool.twitter_search 
-  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do
     member do
@@ -17,6 +16,5 @@ Rails.application.routes.draw do
   end
  
   root to: 'pages#home'
-
   root to: "home#index"
 end
