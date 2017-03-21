d6c529d6c16341fe51a18cf80b82699459015553
Shelbi Schollenberger <sschollenberger6@gmail.com>
2017-03-21 10:57:06 -0600

    Updated blogs_helper.rb to include ruby

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,7 +1,7 @@
 module BlogsHelper
   class CodeRayify < Redcarpet::Render::HTML
     def block_code(code, language)
-      CodeRay.scan(code, language).div
+      CodeRay.scan(code, language ||= :ruby).div
     end
   end 
 
