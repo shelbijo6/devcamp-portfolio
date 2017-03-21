20f1bc6f19f696fecd8fbf39d57fd1906b146ecf
Shelbi Schollenberger <sschollenberger6@gmail.com>
2017-03-21 09:33:56 -0600

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+User.create!(
+  email: "test@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
@@ -15,51 +34,3 @@ puts "3 Topics created"
 end 
 
 puts "10 blog posts created"
-
-5.times do |skill|
-  Skill.create!(
-    title:"Rails #{skill}",
-    percent_utilized: 15
-  )
-end 
-
-puts "5 skills created"
-
-8.times do |portfolio_item|
-  Portfolio.create!(
-    title: "Portfolio Title: #{portfolio_item}",
-    subtitle: "Ruby on Rails",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "https://placehold.it/350x200",
-    thumb_image: "https://placehold.it/300x400"
-  )
-end
-
-1.times do |portfolio_item|
-  Portfolio.create!(
-    title: "Portfolio Title: #{portfolio_item}",
-    subtitle: "Angular",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "https://placehold.it/350x200",
-    thumb_image: "https://placehold.it/350x200"
-  )
-end
-
-puts "9 portfolio items created"
-
-3.times do |technology|
-  Portfolio.last.technologies.create!(
-    name: "Technology #{technology}",
-    portfolio_id: Portfolio.last.id
-  )
-end 
-
-puts "3 technologies created"
-
-
-
-
-
-
-
-
